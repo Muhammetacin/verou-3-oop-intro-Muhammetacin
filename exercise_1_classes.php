80cd3a5c05aac8aad2a1fffd6dfd728b62cc48a9
Muhammet Cin <muhammetacin@gmail.com>
2022-02-22 11:01:06 +0100

    Change Beer properties to private and change color of Duvel to light.

--- exercise_1_classes.php
+++ exercise_1_classes.php
@@ -3,15 +3,15 @@
 declare(strict_types=1);
 
 /* EXERCISE 1
-TODO: Create a class beverage.
-TODO: Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
-TODO: Have a default value "cold" in the construct for temperature.
+Create a class beverage.
+Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
+Have a default value "cold" in the construct for temperature.
 
 Remember for now we will use properties and methods that can be accessed from everywhere.
-TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
-TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
+Make a getInfo function which returns "This beverage is <temperature> and <color>."
+Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
  print the getInfo on the screen.
-TODO: Print the temperature on the screen.
+Print the temperature on the screen.
 
 USE TYPEHINTING EVERYWHERE!
 */
@@ -37,5 +37,5 @@ class Beverage
 
 $cola = new Beverage('black', 2);
 
-echo nl2br($cola->getInfo() . "\n");
-echo 'Temperature of Cola: ' . $cola->temperature;
+//echo nl2br($cola->getInfo() . "\n");
+//echo 'Temperature of Cola: ' . $cola->temperature;
