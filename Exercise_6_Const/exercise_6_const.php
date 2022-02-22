abc018ccb244d549bb5556d5097a0741b6ed1630
Muhammet Cin <muhammetacin@gmail.com>
2022-02-22 16:55:54 +0100

    Remove todos

--- Exercise_6_Const/exercise_6_const.php
+++ Exercise_6_Const/exercise_6_const.php
@@ -6,13 +6,13 @@ declare(strict_types=1);
 
 Copy the classes of exercise 2.
 
-TODO: Change the properties to private.
-TODO: Make a const barname with the value 'Het Vervolg'.
-TODO: Print the constant on the screen.
-TODO: Create a function in beverage and use the constant.
-TODO: Do the same in the beer class.
-TODO: Print the output of these functions on the screen.
-TODO: Make sure that every print is on a new line.
+Change the properties to private.
+Make a const barname with the value 'Het Vervolg'.
+Print the constant on the screen.
+Create a function in beverage and use the constant.
+Do the same in the beer class.
+Print the output of these functions on the screen.
+Make sure that every print is on a new line.
 
 Use typehinting everywhere!
 */
@@ -36,3 +36,7 @@ require '../BeerPrivate.php';
 //        return "Alcohol percentage of beverage is $this->alcoholPercentage";
 //    }
 //}
+
+//$exercise6Duvel = new BeerPrivate('Duvel', 8.5, 'blond', 3.5);
+//print_r($exercise6Duvel->printConstantPropertyFromBeverage() . '<br>');
+//print_r($exercise6Duvel->printConstantPropertyFromBeer() . '<br>');
